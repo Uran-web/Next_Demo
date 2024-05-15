5391e550f864b2768eabaac99813cb2f907fc120
AlexanderMakohon <96062945+AlexanderMakohon@users.noreply.github.com>
2024-05-15 16:58:30 +0200

    fix issues

--- app/page.tsx
+++ app/page.tsx
@@ -10,10 +10,16 @@ export const metadata: Metadata = {
 const getRooms = async (searchParams: string) => {
   const urlParams = new URLSearchParams(searchParams);
   const queryString = urlParams.toString();
-  const req = await fetch(`${process.env.API_URL}/api/rooms?${queryString}`, {
-    cache: 'no-cache',
-  });
-  return req.json();
+
+  try {
+    const req = await fetch(`${process.env.API_URL}/api/rooms?${queryString}`, {
+      cache: 'no-cache',
+    });
+    const data = await req.json();
+    return data;
+  } catch (error) {
+    console.log('error => ', error);
+  }
 };
 
 const HomePage = async ({ searchParams }: { searchParams: string }) => {
